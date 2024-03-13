fc9bfcbbec027da330d821036f79283ecfbed61e
CarrotPeeler <jared24mc@gmail.com>
2024-03-13 14:18:56 -0400

    Added huggingface dataset downloading; subset creation WIP

--- make_sentry_subset.py
+++ make_sentry_subset.py
@@ -3,28 +3,103 @@ import shutil
 import argparse
 from huggingface_hub import hf_hub_download, list_repo_tree
 
+def filter_hidden_files(list_of_files):
+    # filter out hidden files (i.e., .gitattributes)
+    filtered = list(filter(lambda x: x.rpartition('/')[-1][0] != '.', list_of_files))
+    return filtered
+
+
+def filter_redundant_paths(subdirs):
+    # remove redundant/overlapping paths (i.e., home/user/cd is redundant with home/user/cd/disco)
+    to_remove = []
+    for path1 in subdirs:
+        for path2 in subdirs:
+            # check if leaf node from path1 is in path2
+            if path1 != path2 and path1.rpartition('/')[-1] in path2:
+                to_remove.append(path1)
+
+    filtered_subdirs = [path for path in subdirs if path not in to_remove]
+    return filtered_subdirs
+
+
+def filter_imagedata_subset(files, train_reduce_factor, val_reduce_factor):
+    """
+    Creates a subset of files
+        if files are for train data, reduce sample size by train_reduce_factor
+        if files are for val data, reduce sample size by val_reduce_factor
+    args:
+        files: list of tar files for one image data source
+    """
+    files_subset = []
+    if 'MetaData' in files[0] or len(files) == 1: # if metadata or only 1 file, return as is
+        return files
+    elif 'ImageData/train' in files[0]:
+        files_subset = files[0:int(len(files)/train_reduce_factor)]
+    elif 'ImageData/val' in files[0]:
+        files_subset = files[0:int(len(files)/val_reduce_factor)]
+    return files_subset
 
 
 def recreate_file_struct_locally(dataset_path, image_dir_name, metadata_dir_name, repo_id):
+    """Locally reconstructs the folder structure for a dataset repo from HuggingFace Data Servers"""
     if os.path.exists(dataset_path):
         shutil.rmtree(dataset_path)
 
     os.makedirs(dataset_path, exist_ok=True)
 
+    all_subdirs = []
+
     # recreate file structure locally
     for dir in [image_dir_name, metadata_dir_name]:
         for split in ['train', 'val']:
-            subdirs = list(list_repo_tree(repo_id=repo_id, repo_type="dataset", path_in_repo=f'{dir}/{split}'))
-            subdirs = list(map(lambda x:x.path, subdirs))
-            subdirs = list(filter(lambda x: x.rpartition('/')[-1][0] != '.', subdirs)) # filter out hidden files (i.e., .gitattributes)
-            
+            if dir == image_dir_name:
+                subdirs = list(list_repo_tree(repo_id=repo_id, 
+                                              repo_type="dataset", 
+                                              path_in_repo=f'{dir}/{split}',
+                                              recursive=True))
+                subdirs = list(map(lambda x:x.path, subdirs))
+                subdirs = list(filter(lambda x: '.' not in x, subdirs)) # remove any files found
+                subdirs = filter_redundant_paths(subdirs)
+            else:
+                subdirs = [f'{metadata_dir_name}/{split}']
+
             # recreate subdirs locally
             for subdir in subdirs:
                 os.makedirs(f'{dataset_path}/{subdir}', exist_ok=True)
 
-    return subdirs # list of all subdir paths for each data source
+            all_subdirs.append(subdirs)
+
+    return all_subdirs # list of all subdir paths for each data source
 
 
+def download_files(all_subdirs,
+                   repo_id, 
+                   local_dataset_dir, 
+                   train_reduce_factor=4, 
+                   val_reduce_factor=2):
+    for subdirs in all_subdirs:
+        for subdir in subdirs:
+            files = list(list_repo_tree(repo_id=repo_id, 
+                                        repo_type="dataset", 
+                                        path_in_repo=subdir,
+                                        recursive=True))
+            files = list(map(lambda x:x.path, files))
+            files = filter_hidden_files(files)
+
+            # develop subset of files 
+            files = filter_imagedata_subset(files, train_reduce_factor, val_reduce_factor)
+
+            print(files)
+            # for file in files:
+            #     local_save_dir = f"{local_dataset_dir}/{file.rpartition('/')[0]}"
+            #     # download tar files from HuggingFace
+            #     hf_hub_download(repo_id=repo_id, 
+            #                     repo_type="dataset",
+            #                     filename=file,
+            #                     local_dir=local_save_dir)
+            #     os.system(f"tar -xvf {local_save_dir}/{file.rpartition('/')[-1]}")
+
+                # batch convert from png to jpg
 
 
 
@@ -42,22 +117,15 @@ if __name__ == '__main__':
        
     args = parser.parse_args()
 
-    subdirs = recreate_file_struct_locally(args.local_dataset_path, 
+    all_subdirs = recreate_file_struct_locally(args.local_dataset_path, 
                                            args.image_dir, 
                                            args.metadata_dir, 
                                            args.repo_id)
-    print(subdirs)
-    for subdir in subdirs:
-        print(list(list_repo_tree(repo_id="InfImagine/FakeImageDataset", repo_type="dataset", path_in_repo=subdir)))
+    # print(all_subdirs)
+    download_files(all_subdirs, args.repo_id, args.local_dataset_path)
 
 
-
-    # hf_hub_download(repo_id="InfImagine/FakeImageDataset", filename="fleurs.py", repo_type="dataset")
-
-    # p1 = list(list_repo_tree(repo_id="InfImagine/FakeImageDataset", repo_type="dataset", path_in_repo="ImageData"))
-    # p1 = list(map(lambda x: x.path, p1))
-    # print(p1)
-
+   
    
         
     
