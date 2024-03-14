5f752fc1bc9338a8a327ca67847e7727b3c53c32
CarrotPeeler <jared24mc@gmail.com>
2024-03-14 18:42:06 -0400

    added cc3m download scipt

--- README.md
+++ README.md
@@ -9,8 +9,12 @@ pip install -r requirements.txt
 ## Getting the Data
 We use a subset of the [Sentry dataset](https://huggingface.co/datasets/InfImagine/FakeImageDataset) for training and testing models. All images are compressed as JPGs to reduce size and mimic the format used most commonly for uploading images to websites and the internet. 
 
-Run the following script to download, compress, and generate the subset of images and metadata used from Sentry.
+Run the following script to download, compress, and generate the subset of fake images and metadata used from Sentry.
 ```bash
 cd CS545-Real-Fake-Image-Detection
-python3 make_sentry_subset.py < /dev/null > log.txt 2>&1 &
+python3 datagen/make_sentry_subset.py < /dev/null > log.txt 2>&1 &
 ```
+Note: for each dataset in sentry, the respective tar files are downloaded together, extracted, and then deleted to save space. Meaning, you will at most need ~100 GBs to handle the intermediate downloading process. The final compressed subset is only a few GBs. 
+
+For real image data, we use CC3M (Google Conceptual Captions), CelebA-HQ, 
+For CC3M, we use 200k of the train images and 8k of the val images.
---
+++ datagen/add_real_data.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+from img2dataset import download
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--num_cores", type=int, default=10, 
+                        help="number of cpu cores your system has")
+    parser.add_argument("--total_threads", type=int, default=20, 
+                        help="number of cpu threads total")
+    args = parser.parse_args()
+
+    cc3m_train_anno = "datagen/cc3m/Train_GCC-training.tsv"
+    cc3m_val_anno = "datagen/cc3m/Validation_GCC-1.1.0-Validation.tsv"
+    cc3m_train_dir = "./sentry-dataset/ImageData/train/CC3M"
+    cc3m_val_dir = "./sentry-dataset/ImageData/val/CC3M"
+
+    for split in [(cc3m_train_anno, cc3m_train_dir), 
+                  (cc3m_val_anno, cc3m_val_dir)]:
+        os.makedirs(split[1], exist_ok=True) 
+        download(
+            processes_count=args.num_cores,
+            thread_count=args.total_threads,
+            url_list=split[0],
+            image_size=256,
+            output_folder=split[1],
+            output_format="files",
+            encode_format="jpg",
+            input_format="tsv",
+            url_col="url",
+            caption_col="caption",
+            enable_wandb=False,
+            number_sample_per_shard=1000,
+            distributor="multiprocessing",
+        )
