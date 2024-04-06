06e55d72490a9c1402915a452f88f9ca8614e76b
CarrotPeeler <jared24mc@gmail.com>
2024-04-06 13:25:36 -0400

    updated bash scripts

--- UniversalFakeDetect/train_active_learning.sh
+++ UniversalFakeDetect/train_active_learning.sh
@@ -1,3 +1,4 @@
-python3 UniversalFakeDetect/train.py --name=clip_rn50_al --wang2020_data_path=/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ \
+python3 UniversalFakeDetect/train.py --name=clip_rn50_al_max_entropy \
+    --wang2020_data_path=/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ \
     --data_mode=dip --arch=CLIP:RN50 --fix_backbone --save_epoch_freq=5 --gpu_ids=0 --num_threads=4 --batch_size=256 \
-    --use_active_learning --niter=30 --dropout_iter=20 --query=1000 --acq_func=2 < /dev/null > train_al.txt 2>&1 &
+    --use_active_learning --niter=30 --al_iter=20 --dropout_iter=10 --query=2500 --acq_func=2 < /dev/null > train_al_max_entropy.txt 2>&1 &
--- UniversalFakeDetect/train_normal.sh
+++ UniversalFakeDetect/train_normal.sh
@@ -2,5 +2,5 @@ python3 UniversalFakeDetect/train.py \
     --name=clip_vitl14_pretrained \
     --wang2020_data_path=/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ \
     --data_mode=dip  --arch=CLIP:ViT-L/14 --fix_backbone --save_epoch_freq=1 \
-    --gpu_ids=0 --num_threads=4 --batch_size=256 --niter=10 \
-    --ckpt=./UniversalFakeDetect/pretrained_weights/fc_weights.pth < /dev/null > train_normal.txt 2>&1 &
+    --gpu_ids=0 --num_threads=4 --batch_size=256 --niter=10 < /dev/null > train_normal.txt 2>&1 &
+    
